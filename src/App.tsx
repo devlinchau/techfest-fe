f72a6c84e5074aeead030597f6054668eda2a993
Karl Devlin Chau <122105152+devlinchau@users.noreply.github.com>
2024-02-05 23:00:42 +0800

    feat: adds Register

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Chat from './components/Chat';
+import Register from './components/Register';
 
 export default function App() {
   return (
         <div className="flex items-center justify-center h-screen">
-          <Chat />
+          <Register />
         </div>
   );
 };
---
+++ src/components/Register.tsx
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import Chat from './Chat';
+
+export default function Register() {
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password1, setPassword1] = useState<string>("");
+  const [password2, setPassword2] = useState<string>("");
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/api/register/', { username, email, password1, password2 });
+      console.log('Register response:', response.data.response);
+      if(response.data.response === 'Account created successfully!') {
+        alert('Registration successful!');
+        
+      }
+    } catch (error) {
+      console.error('Error registering:', error);
+    } finally {
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-screen w-2/3 md:w-1/2 lg:w-1/3 mx-auto">
+      <div className="flex-grow p-4 overflow-y-auto">
+        <div className="mb-4 py-2">
+          <span className="font-semibold text-slate-700 text-2xl">Register</span>
+        </div>
+        <div className="flex flex-col space-y-4 mb-6">
+          <Input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
+          />
+          <Input
+            placeholder="Confirm Password"
+            type="password"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        </div>
+          <Button
+            type="submit"
+            className='w-1/4 bg-slate-700 hover:bg-slate-600'
+            onClick={handleRegister}
+          >
+            Submit
+          </Button>
+      </div>
+    </div>
+  )
+}
